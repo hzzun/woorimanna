6dca106e931147a7a153b6734bfdb4d7029ea2a9
문현준 <jann1653@gmail.com>
2025-03-22 14:46:41 +0900

    initialize project

---
+++ src/main/kotlin/com/woorimanna/WoorimannaApplication.kt
@@ -0,0 +1,11 @@
+package com.woorimanna
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WoorimannaApplication
+
+fun main(args: Array<String>) {
+    runApplication<WoorimannaApplication>(*args)
+}
---
+++ src/test/kotlin/com/woorimanna/WoorimannaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.woorimanna
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WoorimannaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
